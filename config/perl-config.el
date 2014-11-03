ef13036d298ecf2beb4ae12327a1eac3be4a70a8
Juiko <pulga.p.29@gmail.com>
2014-11-03 00:25:49 -0300

    perl and scheme support

--- config/perl-config.el
+++ config/perl-config.el
@@ -1,18 +1,33 @@
+(defalias 'perl-mode 'cperl-mode)
+
 (use-package auto-complete
 						 :ensure t
 						 :config (progn
 											 (add-hook 'cperl-mode-hook
 																 (lambda ()
-																	 (setf ac-sources
-																				 '(ac-source-words-in-buffer
-																					 ac-source-words-in-same-mode-buffers
-																					 ac-source-etags
-																					 ac-source-abbrev
-																					 ac-source-yasnippet))))
+																	 (loop for source in '(ac-source-words-in-buffer
+																												 ac-source-words-in-same-mode-buffers
+																												 ac-source-etags
+																												 ac-source-abbrev) 
+																				 do (add-to-list 'ac-sources source))))
+											 
+											 (use-package perl-completion
+																		:ensure t
+																		:config (progn
+																							(add-hook 'cperl-mode-hook 
+																												(lambda () 
+																													(add-to-list 'ac-sources 'ac-source-perl-completion)))
+																							(add-hook 'cperl-mode-hook 'perl-completion-mode)))
+
 
 											 (add-hook 'cperl-mode-hook 'auto-complete-mode)))
+
+(use-package yasnippet
+						 :ensure t
+						 :config (progn
+											 (add-hook 'cperl-mode-hook 'yas-minor-mode)))
+
 (use-package flycheck
 						 :ensure t
 						 :config (progn
 											 (add-hook 'cperl-mode-hook 'flycheck-mode)))
-(provide 'perl-config)
--- config/scheme-config.el
+++ config/scheme-config.el
@@ -1,6 +1,18 @@
-(use-package quack
-  :commands scheme-mode
-  :config (progn
-            (setq scheme-program-name "guile")))
+(use-package geiser
+						 :ensure t
+						 :config (progn
+											 (add-hook 'geiser-mode 'geiser-autodoc-mode)
+											 (add-hook 'scheme-mode 'turn-on-geiser-mode)))
+
+(use-package company
+						 :ensure t
+						 :config (progn
+											 (add-hook 'geiser-mode 
+																 (lambda () (company-mode t)))))
+
+(use-package evil
+						 :ensure t
+						 :config (progn
+											 (evil-set-initial-state 'geiser-repl-mode 'emacs)))
 
 (provide 'scheme-config)
