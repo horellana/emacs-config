ec0642b06fc3be1656d3c5f4cdd871e1c989ee83
Juiko <pulga.p.29@gmail.com>
2014-11-03 21:39:23 -0300

    cursor color

--- config/look-config.el
+++ config/look-config.el
@@ -1,12 +1,18 @@
 (require 'cl)
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
 
-(add-to-list 'default-frame-alist '(font . "Droid Sans Mono-10"))
+(use-package leuven-theme
+						 :ensure t
+						 :config (progn
+											 (load-theme 'leuven t)
+											 
+											 (set-face-attribute 'fringe
+																					 nil
+																					 :background "white"
+																					 :foreground "white")))
 
 (use-package color-theme
+						 :disabled t
 						 :ensure t
 						 :config (progn
 											 (load "/home/juiko/git/color-theme-mac-classic/color-theme-mac-classic.el")
@@ -17,7 +23,7 @@
 						 :config (use-package powerline-evil
 																	:ensure t
 																	:config (powerline-evil-vim-color-theme)))
-																	
+
 (use-package diminish
 						 :defer t
 						 :ensure t
@@ -32,5 +38,17 @@
 																						whitespace-mode
 																						flycheck-mode)
 															do (ignore-errors (diminish mode))))
+
+(progn
+	(menu-bar-mode -1)
+	(tool-bar-mode -1)
+	(scroll-bar-mode -1)
+
+	(add-to-list 'default-frame-alist '(font . "Droid Sans Mono-10"))
+
+	(set-display-table-slot standard-display-table 
+													'vertical-border (make-glyph-code 8203))
+
+	(set-cursor-color "gray"))
 
 (provide 'look-config)
