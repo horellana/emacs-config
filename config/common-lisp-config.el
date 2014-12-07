5f1fd9b62d6188d66174f8dd23d8de2d6324a477
Juiko <pulga.p.29@gmail.com>
2014-12-06 21:51:33 -0300

    paredit and more stuff ...

--- config/common-lisp-config.el
+++ config/common-lisp-config.el
@@ -1,23 +1,42 @@
-(setq inferior-lisp-program "sbcl")
+(setq inferior-lisp-program "/home/juiko/git/sbcl/run-sbcl.sh")
 (setq slime-contribs '(slime-fancy slime-company))
-
-(use-package slime-company
-	:ensure t)
-
-(use-package slime
-	:ensure t)
-
-(use-package aggressive-indent
-	:ensure t
-	:config (progn
-						(add-hook 'lisp-mode-hook 'aggressive-indent-mode)))
 
 (use-package evil-leader
 	:ensure t
 	:config (progn
+						(add-hook 'lisp-mode-hook 'evil-leader-mode)
 						(evil-leader/set-key-for-mode 'lisp-mode "cl" 'slime-load-file)
 						(evil-leader/set-key-for-mode 'lisp-mode "e" 'slime-eval-last-expression)
 						(evil-leader/set-key-for-mode 'lisp-mode "me" 'slime-macroexpand-1)
 						(evil-leader/set-key-for-mode 'lisp-mode "ma" 'slime-macroexpand-all)))
+
+(use-package aggressive-indent
+	:ensure t
+	:config (progn (add-hook 'lisp-mode-hook 
+													 (lambda () (aggressive-indent-mode t)))))
+
+(use-package paredit
+	:ensure t
+	:config (progn (add-hook 'lisp-mode-hook 
+													 (lambda () (paredit-mode t)))))
+
+(use-package evil-paredit
+	:ensure t
+	:config (progn (add-hook 'lisp-mode-hook 
+													 (lambda () (evil-paredit-mode t)))))
+
+(use-package evil-lisp-state
+	:ensure t)
+
+(use-package slime
+	:ensure t
+	:config (progn
+						(add-hook 'lisp-mode-hook
+											(lambda ()
+												(unless (slime-connected-p)
+													(save-excursion (slime)))))))
+
+(use-package slime-company
+	:ensure t)
 
 (provide 'common-lisp-config)
