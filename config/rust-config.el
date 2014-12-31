b4abead55a37ec9c98b568f07efeb8173afee62d
Juiko <pulga.p.29@gmail.com>
2014-12-31 13:58:54 -0300

    Rust lang config

--- config/rust-config.el
+++ config/rust-config.el
@@ -1,4 +1,28 @@
-(add-hook 'rust-mode-hook 'company-mode)
-(add-hook 'rust-mode-hook 'flycheck-mode)
+(use-package rust-mode
+	:ensure t)
+
+(use-package company
+	:ensure t
+	:config (progn
+						(add-hook 'rust-mode-hook 'company-mode)))
+
+(use-package flycheck
+	:ensure t
+	:config (progn
+						(add-hook 'rust-mode-hook 'flycheck-mode)))
+
+(use-package aggressive-indent
+	:ensure t
+	:config (progn
+						(add-hook 'rust-mode-hook 'aggressive-indent-mode)))
+
+(use-package emacs-rust-helpers)
+
+(progn
+	(add-to-list 'load-path "/home/juiko/git/racer/editors")
+	(use-package racer
+		:commands rust-mode
+		:config (setq racer-rust-src-path "/home/juiko/git/rust/src/"
+									racer-cmd "/home/juiko/git/racer/target/release/racer")))
 
 (provide 'rust-config)
