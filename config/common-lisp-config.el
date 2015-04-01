82c1b6e343082cba27df1ca3e797672148d7ef8a
Juiko <pulga.p.29@gmail.com>
2015-04-01 15:05:16 -0300

    Be in emacs-state whe doing a macroexpand

--- config/common-lisp-config.el
+++ config/common-lisp-config.el
@@ -6,46 +6,51 @@
 											 inferior-slime))
 
 (use-package evil
-	:ensure t
-	:config (progn
+  :ensure t
+  :config (progn
 						(evil-set-initial-state 'slime-trace-dialog-mode 'emacs)))
 
 (use-package evil-leader
-	:ensure t
-	:config (progn
-						(add-hook 'lisp-mode-hook 'evil-leader-mode)
-						(evil-leader/set-key-for-mode 'lisp-mode "cl" 'slime-load-file)
-						(evil-leader/set-key-for-mode 'lisp-mode "e" 'slime-eval-last-expression)
-						(evil-leader/set-key-for-mode 'lisp-mode "me" 'slime-macroexpand-1)
-						(evil-leader/set-key-for-mode 'lisp-mode "ma" 'slime-macroexpand-all)))
+  :ensure t
+  :config (progn
+	    (add-hook 'lisp-mode-hook 'evil-leader-mode)
+	    (evil-leader/set-key-for-mode 'lisp-mode "cl" 'slime-load-file)
+	    (evil-leader/set-key-for-mode 'lisp-mode "e" 'slime-eval-last-expression)
+	    (evil-leader/set-key-for-mode 'lisp-mode "me" 'slime-macroexpand-1)
+	    (evil-leader/set-key-for-mode 'lisp-mode "ma" 'slime-macroexpand-all)))
 
 (use-package aggressive-indent
-	:ensure t
-	:config (progn (add-hook 'lisp-mode-hook 
-													 (lambda () (aggressive-indent-mode t)))))
+  :ensure t
+  :config (progn (add-hook 'lisp-mode-hook 
+			   (lambda () (aggressive-indent-mode t)))))
 
 (use-package paredit
-	:ensure t
-	:config (progn (add-hook 'lisp-mode-hook 
-													 (lambda () (paredit-mode t)))))
+  :ensure t
+  :config (progn (add-hook 'lisp-mode-hook 
+			   (lambda () (paredit-mode t)))))
 
 (use-package evil-paredit
-	:ensure t
-	:config (progn (add-hook 'lisp-mode-hook 
-													 (lambda () (evil-paredit-mode t)))))
+  :ensure t
+  :config (progn (add-hook 'lisp-mode-hook 
+			   (lambda () (evil-paredit-mode t)))))
 
 (use-package evil-lisp-state
-	:ensure t)
+  :ensure t)
 
 (use-package slime
-	:ensure t
-	:config (progn
+  :ensure t
+  :config (progn
 						(add-hook 'lisp-mode-hook
 											(lambda ()
 												(unless (slime-connected-p)
-													(save-excursion (slime)))))))
+													(save-excursion (slime)))))
+						(add-hook 'lisp-mode-hook (lambda ()
+																				(slime-mode t)))
+
+						(add-hook 'slime-macroexpansion-minor-mode-hook
+											'evil-emacs-state)))
 
 (use-package slime-company
-	:ensure t)
+  :ensure t)
 
 (provide 'common-lisp-config)
