769ec5ce49fdb88ffa4754ef1049b1f60a028d91
Hector Orellana <pulga.p.29@gmail.com>
2016-03-16 20:42:02 -0300

    anaconda-mode and company-anaconda

--- init.el
+++ init.el
@@ -449,8 +449,11 @@
   :config (progn
 	    (add-hook 'haskell-mode-hook
 		      (lambda ()
-			(setq-local company-backends '(company-ghci))))
-	    ))
+			(setq-local company-backends
+				    '(company-ghci
+				      company-dabbrev
+				      company-dabbrev-code))))))
+
 
 (req-package hlint-refactor
   :require haskell-mode
@@ -465,9 +468,13 @@
 
 (req-package company-anaconda
   :require company anaconda-mode
-  :commands (anaconda-mode)
   :config (progn
-	    (add-to-list 'company-backends 'company-anaconda)))
+	    (add-hook 'anaconda-mode-hook
+		      (lambda ()
+			(make-variable-buffer-local 'company-backends)
+			(setq-local company-backends '(company-anaconda))))
+	    ;; (add-to-list 'company-backends 'company-anaconda)
+	    ))
 
 (req-package pyvenv
   :config (progn
@@ -559,3 +566,17 @@
 (req-package-finish)
 
 (juiko/look-config)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet web-mode tao-theme slime-company req-package railscasts-theme racer pyvenv php-mode paper-theme leuven-theme js2-mode irony-eldoc iedit hlint-refactor hindent helm-swoop helm-projectile helm-ag greymatters-theme flycheck-rust flycheck-pos-tip flycheck-irony flycheck-haskell flycheck-elm evil-smartparens evil-magit evil-lisp-state evil-leader evil-god-state evil-commentary elm-mode company-quickhelp company-irony company-ghci company-anaconda badwolf-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
