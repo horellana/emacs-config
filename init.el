5b80d1685bcfc1c5883b029b81245ca324b09b0c
Hector Orellana <pulga.p.29@gmail.com>
2016-10-11 19:36:13 -0300

    eldoc-mode for robe-mode

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 (setf lexical-binding t)
 
-(setq gc-cons-threshold 100000000)
+(setf gc-cons-threshold 100000000)
 
 (eval-when-compile
   (require 'cl))
@@ -10,13 +10,22 @@
 
 (progn
   (add-to-list 'package-archives
-	       '("melpa" . "http://melpa.org/packages/") t)
+	       '("melpa" . "http://melpa.org/packages/"))
+  (add-to-list 'package-archives
+	       '("elpy" . "https://jorgenschaefer.github.io/packages/"))
   (package-initialize)
 
   (when (not (package-installed-p 'req-package))
     (package-refresh-contents)
     (package-install 'req-package)))
 (require 'req-package)
+
+(defun juiko/compile-emacs-home ()
+  (f-files "~/.emacs.d/"
+	   (lambda (f) (when (string-suffix-p ".el" f)
+			 (byte-compile-file f)))
+	   t))
+
 
 (defun juiko/look-config ()
   (blink-cursor-mode -1)
@@ -26,17 +35,17 @@
   (column-number-mode 1)
   (global-hl-line-mode 1)
   (show-paren-mode)
-  (add-to-list 'default-frame-alist '(font . "mononoki-9"))
+  (add-to-list 'default-frame-alist '(font . "Hack-8"))
   (add-to-list 'default-frame-alist '(cursor-color . "Gray")))
 
 (defvar *python-env-name*
   (list "env" ".env"))
 
 (defun python-find-env (project-root)
-  (let ((found-env (-intersection *python-env-name*
-				  (mapcar 'f-filename (f-directories project-root)))))
-    (when (car found-env))))
-  
+  (car (-intersection (mapcar (lambda (path) (f-join project-root path))
+			      *python-env-name*)
+		      (f-directories project-root))))
+
 (let (python-current-env)
   (add-hook 'python-mode-hook
 	    (lambda ()
@@ -46,11 +55,12 @@
 			   (not (equal env
 				       python-current-env)))
 		  (setf python-current-env env)
-		  (pyvenv-activate env))))))
+		  (pyvenv-activate env)
+		  (message "Current python env: %s" python-current-env))))))
 
 (defun gtags-exists-p (root)
   (-contains-p  (f-files root)
-		(concat root "GTAGS")))
+		(f-join root "GTAGS")))
 
 (defun async-gtags-create ()
   (call-process "gtags" nil 0 nil))
@@ -61,20 +71,22 @@
 (defun async-gtags (root)
   (if (gtags-exists-p root)
       (async-gtags-update)
-    (async-gtags-create)))
+    (let ((default-directory root))
+      (async-gtags-create))))
 
-(defvar *gtags-modes*
-  '(web-mode
-    php-mode
-    cperl-mode
-    ruby-mode))
+(setf *gtags-modes*
+      '(web-mode
+	php-mode
+	cperl-mode
+	ruby-mode
+	))
 
 (add-hook 'after-save-hook
 	  (lambda ()
 	    (if (cl-member major-mode *gtags-modes*)
-		;; (async-gtags-update (projectile-project-root))
 		(async-gtags (projectile-project-root)))))
 
+(add-hook 'after-save-hook 'whitespace-cleanup)
 
 (defun endless/upgrade ()
   "Upgrade all packages, no questions asked."
@@ -86,10 +98,10 @@
 
 (add-hook 'prog-mode-hook 'eldoc-mode)
 
-(setq inhibit-startup-message t)
+(setf inhibit-startup-message t)
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(setq backup-by-copying t      ; don't clobber symlinks
+(setf backup-by-copying t      ; don't clobber symlinks
       backup-directory-alist
       '(("." . "~/.emacs.d/saves"))    ; don't litter my fs tree
       delete-old-versions t
@@ -103,15 +115,17 @@
 	      (when (equal (buffer-file-name) init-file)
 		(byte-compile-file init-file)))))
 
-(setq browse-url-browser-function 'browse-url-firefox)
+(progn
+  (setf browse-url-browser-function 'browse-url-firefox)
+  (setf browse-url-firefox-program "firefox-aurora"))
 
 (progn
   (defalias 'perl-mode 'cperl-mode)
-  (setq cperl-electric-parens nil
+  (setf cperl-electric-parens nil
 	cperl-electric-keywords nil
 	cperl-electric-lbrace-space nil))
 
-(setq backup-directory-alist
+(setf backup-directory-alist
       '((".*" . "/home/juiko/.emacs.d/cache/"))
       auto-save-file-name-transforms
       '((".*" "/home/juiko/.emacs.d/cache/" t))
@@ -120,7 +134,25 @@
 
 (juiko/look-config)
 
+(req-package irony
+  :config (progn
+	    (add-hook 'c-mode-hook 'irony-mode)
+	    (add-hook 'c++-mode-hook 'irony-mode)
+	    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)))
 
+(req-package company-irony
+  :commands (irony-mode)
+  :config (progn
+	    (add-hook 'irony-mode-hook
+		      (lambda ()
+			(setq-local company-backends '(company-irony))))))
+
+(req-package flycheck-irony
+  :config (add-hook 'flycheck-mode-hook 'flycheck-irony-setup))
+
+(req-package irony-eldoc
+  :config (progn
+	    (add-hook 'irony-mode-hook 'irony-eldoc)))
 
 (req-package f)
 
@@ -150,16 +182,20 @@
 
 (req-package company
   :config (progn
-	    (setq company-minimum-prefix-length 3)
-	    (setq company-show-numbers t)
+	    (setf company-minimum-prefix-length 2)
+	    (setf company-show-numbers t)
+	    (setf company-idle-delay 1)
+	    (setf company-quickhelp-delay 1)
 	    (global-company-mode)))
 
 (req-package company-quickhelp
+  :disabled t
   :if window-system
   :require company
   :config (add-hook 'company-mode-hook 'company-quickhelp-mode))
 
 (req-package flycheck
+
   :config (progn
 	    (setf flycheck-perlcritic-severity 5)
 	    (setf flycheck-ghc-args (list
@@ -175,6 +211,13 @@
 				     "-fwarn-missing-exported-sigs"
 				     "-fwarn-identities"
 				     "-Wall"))
+
+	    (custom-set-faces
+	     '(flycheck-error ((t (:underline "Red1"))))
+	     '(flycheck-info ((t (:underline "ForestGreen"))))
+	     '(flycheck-warning ((t (:underline "DarkOrange"))))
+	     )
+
 	    (global-flycheck-mode)))
 
 (req-package flycheck-pos-tip
@@ -188,6 +231,7 @@
   '(haskell-mode))
 
 (req-package smartparens
+
   :config (progn
 	    (sp-local-pair '(emacs-lisp-mode
 			     lisp-mode
@@ -205,21 +249,20 @@
 			  (smartparens-strict-mode))))))
 
 (req-package slime-company
+  :disabled t
   :require company)
 
 (req-package slime
-  :require slime-company
-  :commands (slime)
   :init (progn
-	  (setq inferior-lisp-program "sbcl")
-	  (setq slime-contrib '(slime-fancy
-				slime-company))
-	  (setq slime-sbcl-manual-root "/usr/local/share/info/sbcl.info")
+	  (setf inferior-lisp-program "sbcl")
+	  (setf slime-contrib '(slime-fancy slime-company))
+	  (setf slime-sbcl-manual-root "/usr/local/share/info/sbcl.info")
 	  (add-hook 'lisp-mode-hook
 		    (lambda ()
 		      (unless (slime-connected-p)
 			(save-excursion (slime))))))
   :config (progn
+	    (require 'slime-company)
 	    (slime-setup '(slime-fancy slime-company))))
 
 (req-package evil
@@ -233,7 +276,7 @@
 				   (newline)))
 		      evil-normal-state-map)
 
-	    (setq evil-move-cursor-back nil)
+	    (setf evil-move-cursor-back nil)
 
 	    (cl-loop for mode in '(haskell-interactive-mode
 				   haskell-presentation-mode
@@ -258,8 +301,8 @@
 (req-package evil-lisp-state
   :require evil evil-leader bind-key
   :init (progn
-	  (setq evil-lisp-state-global t)
-	  (setq evil-lisp-state-enter-lisp-state-on-command nil))
+	  (setf evil-lisp-state-global t)
+	  (setf evil-lisp-state-enter-lisp-state-on-command nil))
   :config (progn
 	    (bind-key "L" 'evil-lisp-state evil-normal-state-map)
 	    ))
@@ -283,7 +326,7 @@
 (req-package evil-leader
   :require evil
   :config (progn
-	    (setq evil-leader/leader (kbd ","))
+	    (setf evil-leader/leader (kbd ","))
 	    (evil-leader/set-key
 	      "f" 'helm-find-files
 	      "b" 'switch-to-buffer
@@ -326,9 +369,20 @@
   :require evil magit
   )
 
+
+;; (req-package material-theme)
+
+
+;; (progn
+;;   (load-theme 'wombat t)
+;;   (set-face-attribute 'fringe
+;;		      nil
+;;		      :background "ffffff"
+;;		      :foreground "ffffff"))
+
 (req-package leuven-theme
-  :if window-system
   :disabled t
+  :if window-system
   :config (progn
 	    (add-hook 'after-init-hook
 		      (lambda ()
@@ -349,7 +403,7 @@
 	    (bind-key "M-x" 'helm-M-x)
 	    (bind-key "C-x C-f" 'helm-find-files)
 
-	    (setq helm-split-window-in-side-p t)
+	    (setf helm-split-window-in-side-p t)
 
 	    (add-to-list 'display-buffer-alist
 			 '("\\`\\*helm.*\\*\\'"
@@ -357,7 +411,7 @@
 			   (inhibit-same-window . t)
 			   (window-height . 0.4)))
 
-	    (setq helm-swoop-split-with-multiple-windows nil
+	    (setf helm-swoop-split-with-multiple-windows nil
 		  helm-swoop-split-direction 'split-window-vertically
 		  helm-swoop-split-window-function 'helm-default-display-buffer)
 
@@ -380,9 +434,13 @@
   :require helm
   :commands (helm-ack))
 
+(req-package helm-gtags
+  :require helm
+  :config (add-hook 'prog-mode-hook 'helm-gtags-mode))
+
 (req-package magit
   :init (progn
-	  (setq magit-last-seen-setup-instructions "1.4.0")))
+	  (setf magit-last-seen-setup-instructions "1.4.0")))
 
 (req-package haskell-mode
   :config (progn
@@ -394,40 +452,55 @@
 	    (add-hook 'haskell-mode-hook (lambda ()
 					   (electric-indent-local-mode -1)))
 
-	    (setq haskell-process-type 'stack-ghci)
-	    (setq haskell-process-path-ghci "stack")
-	    (setq haskell-process-args-ghci '("ghci"))
+	    (setf haskell-process-type 'stack-ghci)
+	    (setf haskell-process-path-ghci "stack")
+	    (setf haskell-process-args-ghci '("ghci"))
 
-	    (setq haskell-process-suggest-remove-import-lines t)
-	    (setq haskell-process-auto-import-loaded-modules t)
-	    (setq haskell-process-log nil)
-	    (setq haskell-stylish-on-save t)))
+	    (setf haskell-process-suggest-remove-import-lines t)
+	    (setf haskell-process-auto-import-loaded-modules t)
+	    (setf haskell-process-log nil)
+	    (setf haskell-stylish-on-save t)))
 
+(req-package intero
+  :require haskell-mode flycheck
+  :config (progn
+	    (flycheck-add-next-checker 'intero '(warning . haskell-hlint))
+
+	    (add-hook 'haskell-mode-hook 'intero-mode)
+	    (add-hook 'intero-mode-hook
+		      (lambda ()
+			(progn
+			  (make-variable-buffer-local 'company-backends)
+			  (setq-local company-backends '(company-intero)
+				      ;; '((company-intero
+				      ;;	 company-dabbrev-code))
+				      ))
+			))))
 
 (req-package hindent
   :require haskell-mode
   :config (progn
-	    (setq hindent-style "chris-done")
+	    (setf hindent-style "chris-done")
 	    (evil-define-key 'evil-visual-state hindent-mode-map "TAB"
 	      'hindent-reformat-region)
 	    (add-hook 'haskell-mode-hook 'hindent-mode)))
 
 (req-package flycheck-haskell
   :require flycheck haskell-mode
+  :disabled t
   :config (progn
 	    (add-hook 'haskell-mode-hook 'flycheck-mode)
 	    (add-hook 'flycheck-mode-hook 'flycheck-haskell-configure)))
 
 (req-package company-ghci
+  :disabled t
   :require company haskell-mode
   :config (progn
 	    (add-hook 'haskell-mode-hook
 		      (lambda ()
 			(setq-local company-backends
 				    '((company-ghci
-				       company-dabbrev
 				       company-dabbrev-code)))))))
-
 
 (req-package hlint-refactor
   :require haskell-mode
@@ -435,6 +508,20 @@
 	    (bind-key "C-c h r" 'hlint-refactor-refactor-at-point hlint-refactor-mode-map)
 
 	    (add-hook 'haskell-mode-hook 'hlint-refactor-mode)))
+
+(req-package ghc-mod
+  :disabled t
+  :require haskell-mode
+  :init (progn
+	  (autoload 'ghc-init "ghc" nil t)
+	  (autoload 'ghc-debug "ghc" nil t)
+	  (add-hook 'haskell-mode-hook (lambda () (ghc-init)))))
+
+(req-package company-ghc
+  :disabled t
+  :require haskell-mode ghc-mod company
+  :config (progn
+	    (add-to-list 'company-backends 'company-ghc)))
 
 (req-package anaconda-mode
   :config (progn
@@ -447,8 +534,13 @@
 	    (add-hook 'anaconda-mode-hook
 		      (lambda ()
 			(make-variable-buffer-local 'company-backends)
-			(setq-local company-backends '(company-anaconda company-dabbrev-code))))
-	    ))
+			(setq-local company-backends '(company-anaconda))))))
+
+(req-package elpy
+  :disabled t
+  :config (progn
+	    (elpy-enable)))
+
 
 (req-package pyvenv
   :config (progn
@@ -460,44 +552,24 @@
 	    (add-hook 'python-mode-hook 'yas-minor-mode)
 	    (add-hook 'ruby-mode-hook 'yas-minor-mode)))
 
-(req-package irony
-  :disabled t
-  :config (progn
-	    (add-hook 'c-mode-hook 'irony-mode)
-	    (add-hook 'c++-mode-hook 'irony-mode)
-	    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)))
-
-(req-package company-irony
-  :require company irony
-  :commands (irony-mode)
-  :config (progn
-	    (push 'company-irony company-backends)))
-
-(req-package flycheck-irony
-  :require flyckeck irony
-  :config (add-hook 'flycheck-mode-hook 'flycheck-irony-setup))
-
-(req-package irony-eldoc
-  :require irony
-  :config (progn
-	    (add-hook 'irony-mode-hook 'eldoc-mode)))
 
 
 (req-package web-mode
+
   :config (progn
 	    (add-hook 'web-mode-hook #'turn-off-smartparens-mode)
 
 	    (add-hook 'web-mode-hook
 		      (lambda ()
-			(setq web-mode-enable-auto-pairing t)
-			(setq web-mode-enable-css-colorization t)
-			(setq web-mode-enable-block-face t)
-			(setq web-mode-enable-heredoc-fontification t)
-			(setq web-mode-enable-current-element-highlight nil)
-			(setq web-mode-enable-current-column-highlight nil)
-			(setq web-mode-code-indent-offset 2)
-			(setq web-mode-markup-indent-offset 2)
-			(setq web-mode-css-indent-offset 2)))
+			(setf web-mode-enable-auto-pairing t)
+			(setf web-mode-enable-css-colorization t)
+			(setf web-mode-enable-block-face t)
+			(setf web-mode-enable-heredoc-fontification t)
+			(setf web-mode-enable-current-element-highlight nil)
+			(setf web-mode-enable-current-column-highlight nil)
+			(setf web-mode-code-indent-offset 2)
+			(setf web-mode-markup-indent-offset 2)
+			(setf web-mode-css-indent-offset 2)))
 
 	    (cl-loop
 	     for extension in '("\\.blade\\.php\\'"
@@ -509,14 +581,17 @@
 				"\\.mustache\\'"
 				"\\.djhtml\\'"
 				"\\.html\\'"
-				"\\html\\.twig\\'")
+				"\\html\\.twig\\'"
+				"\\pdf\\.twig\\'")
 	     do (add-to-list 'auto-mode-alist `(,extension . web-mode)))))
 
 
 (req-package php-mode
+
   :config (progn
 	    (require 'php-ext)
-	    (setq php-template-compatibility nil)
+	    (setf php-template-compatibility nil)
+	    (setf php-lineup-cascaded-calls t)
 	    (add-hook 'php-mode-hook
 		      'php-enable-symfony2-coding-style)
 
@@ -524,36 +599,29 @@
 		      (lambda ()
 			(setq-local company-backends '(company-gtags
 						       company-dabbrev-code
-						       company-dabbrev
 						       ))))
 	    (with-eval-after-load "yasnippet"
 	      (add-hook 'php-mode-hook 'yas-minor-mode))))
 
-(req-package ac-php
-  :disabled t
-  :requires php-mode
-  :config (progn
-	    (require 'ac-php-company)
-	    (add-hook 'php-mode-hook
-		      (lambda ()
-			(setq-local company-backends '((company-ac-php-backend
-							company-dabbrev-code)))))))
-
 (req-package php-eldoc
-  :requires php-mode)
+  :require php-mode
+  :config (progn
+	    (add-hook 'php-mode-hook 'php-eldoc-enable)))
 
 (req-package js2-mode
+
   :config (progn
 	    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))))
 
 (req-package rust-mode
+
   )
 
 (req-package racer
   :require rust-mode company
   :config (progn
-	    (setq racer-cmd "/home/juiko/git/racer/target/release/racer")
-	    (setq racer-rust-src-path "/home/juiko/git/rust/src/")
+	    (setf racer-cmd "/home/juiko/git/racer/target/release/racer")
+	    (setf racer-rust-src-path "/home/juiko/git/rust/src/")
 	    (add-hook 'rust-mode-hook 'racer-mode)
 	    (add-hook 'racer-mode-hook 'eldoc-mode)
 	    ))
@@ -563,7 +631,8 @@
   :config (progn
 	    (add-hook 'flycheck-mode-hook 'flycheck-rust-setup)))
 
-(req-package elm-mode)
+(req-package elm-mode
+  )
 
 (req-package flycheck-elm
   :require elm-mode flycheck
@@ -575,15 +644,18 @@
   :config (progn
 	    (color-theme-approximate-on)))
 
+
 (req-package robe
   :config (progn
 	    (add-hook 'ruby-mode-hook 'robe-mode)
-	    (add-hook 'ruby-mode-hook
+	    (add-hook 'robe-mode-hook
 		      (lambda ()
 			(make-variable-buffer-local 'company-backends)
-			(setq-local company-backends '(company-robe
-						       company-dabbrev
-						       company-dabbrev-code))))))
+			(setq-local company-backends '((company-robe
+							company-gtags
+							company-dabbrev
+							company-dabbrev-code)))))
+	    (add-hook 'robe-mode-hook 'eldoc-mode)))
 
 (req-package rbenv
   :require robe
@@ -591,9 +663,36 @@
 	    (global-rbenv-mode)))
 
 
-(req-package cider)
+(req-package projectile-rails
+  :require projectile
+  :config (progn
+	    (add-hook 'projectile-mode-hook 'projectile-rails-on)))
+
+(req-package cider
+  :config (progn
+	    (add-hook 'clojure-mode-hook 'cider-mode))
+  )
+
+(req-package yaml-mode
+  )
+
+(req-package tide
+
+  :config (progn
+	    (add-hook 'typescript-mode-hook  'tide-setup)
+	    ))
+
+(req-package dumb-jump
+  :config (progn
+	    (dumb-jump-mode)))
 
 (req-package-finish)
+
+(use-package spacemacs-light-theme
+  :disabled t
+  :load-path "/home/juiko/git/spacemacs-theme"
+  :config (progn
+	    (load-theme 'spacemacs-light t)))
 
 
 (use-package tao-yang-theme
@@ -601,8 +700,26 @@
   :config (progn
 	    (load-theme 'tao-yang t)))
 
-;; (load "~/.emacs.d/tao-theme-emacs/tao-yang-theme.el")
+;; (use-package go-eldoc)
 
-;; (load-theme 'tao-yang t)
-
-
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-error ((t (:underline "Red1"))))
+ '(flycheck-info ((t (:underline "ForestGreen"))))
+ '(flycheck-warning ((t (:underline "DarkOrange")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-safe-themes
+   (quote
+    ("c5d6d43251953cea30a007acf7a6fbca71384558a11fe3d95953ac056a8510dc" default)))
+ '(package-selected-packages
+   (quote
+    (spacemacs-theme docker dockerfile-mode coffee-mode company-ghc elpy yaml-mode web-mode tide slime-company robe req-package rbenv racer pyvenv php-eldoc org monokai-theme material-theme leuven-theme js2-mode irony-eldoc intero iedit hlint-refactor hindent helm-swoop helm-projectile helm-gtags helm-ag go-mode flycheck-rust flycheck-pos-tip flycheck-irony flycheck-haskell flycheck-elm evil-smartparens evil-magit evil-lisp-state evil-leader evil-god-state evil-commentary elm-mode dumb-jump csv-mode company-quickhelp company-irony company-ghci company-anaconda color-theme-approximate cider arjen-grey-theme ac-php))))
