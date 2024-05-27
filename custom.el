63fa7b1a574edd31aa9f3804c6fee36039c49ba5
Hector Orellana <h.orellana@samtech.cl>
2024-05-27 14:30:07 -0400

    custom.el

--- custom.el
+++ custom.el
@@ -496,14 +496,14 @@
          "~/Documents/org-mode/journal") nil nil "Customized with use-package evil-org")
  '(package-selected-packages
    '(all-the-icons almost-mono-themes cape cider consult-project-extra
-                   corfu doom-modeline doom-themes ef-themes
-                   eglot-booster eldoc-box embark-consult
-                   evil-commentary evil-god-state evil-leader
-                   evil-lisp-state evil-org gcmh go-mode haskell-mode
-                   helpful kind-icon kotlin-mode magit marginalia
-                   no-littering orderless org-journal platformio-mode
-                   quelpa-use-package rust-mode sly treesit-auto
-                   vertico which-key yaml-mode yasnippet-snippets))
+                   corfu doom-themes ef-themes eglot-booster eldoc-box
+                   embark-consult evil-commentary evil-god-state
+                   evil-leader evil-lisp-state evil-org gcmh go-mode
+                   haskell-mode helpful kind-icon kotlin-mode magit
+                   marginalia no-littering orderless org-journal
+                   platformio-mode quelpa-use-package rust-mode sly
+                   treesit-auto vertico which-key yaml-mode
+                   yasnippet-snippets))
  '(package-vc-selected-packages
    '((eglot-booster :vc-backend Git :url
                     "https://github.com/jdtsmith/eglot-booster")))
